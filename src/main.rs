47cde88dae2fb332f0c0ba2e8cfa51bec0dea9b8
bukvic6 <bukvic6@gmail.com>
2023-08-31 23:35:45 +0200

    Scaling image using Nearest Neighbor algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use image::{ImageResult, RgbImage};
 use serde::Deserialize;
 use serde_json::Result as SerdeResoult;
-use image::{ImageResult};
 use std::fs;
 use std::io;
 
@@ -26,12 +26,11 @@ fn main() -> SerdeResoult<()> {
     println!("\n---------------------------------------------------------------\n");
     println!("\nRemember, you're arranging the numbers from most to least likely\n");
 
-
     let mut new_one_dim: Vec<u8> = Vec::with_capacity(3 * WIDTH * HEIGHT);
     for question in quiz_data.iter() {
-        println!("{:?}", question.question);
+        println!("{}", question.question);
         for ans in question.answer.iter() {
-            println!("{:?}", ans.text);
+            println!("   {}", ans.text);
         }
         loop {
             let mut input = String::new();
@@ -50,7 +49,7 @@ fn main() -> SerdeResoult<()> {
                         .iter()
                         .all(|value| parsed_values.contains(value))
                     {
-                        println!("must enter numbers from 1 to 3");
+                        println!("Must enter numbers from 1 to 3");
                         continue;
                     }
                     println!("Choose number from 0 to 850");
@@ -59,7 +58,14 @@ fn main() -> SerdeResoult<()> {
                         .read_line(&mut random_number)
                         .expect("Failed to read line");
                     let random_number: u32 = match random_number.trim().parse() {
-                        Ok(num) => num,
+                        Ok(num) => {
+                            if num <= 850 {
+                                num
+                            } else {
+                                eprintln!("Number must be between 0 and 850.");
+                                continue;
+                            }
+                        },
                         Err(_) => continue,
                     };
                     let n = 3;
@@ -72,18 +78,51 @@ fn main() -> SerdeResoult<()> {
                     break;
                 }
                 Err(e) => {
-                    eprintln!("Error parsing input: {}", e);
+                    println!("{}", e);
                     continue;
                 }
             }
         }
     }
-    let result: ImageResult<()> = image::save_buffer("quizzi_image.png", &new_one_dim, WIDTH as u32, HEIGHT as u32, image::ColorType::Rgb8);
+    let result: ImageResult<()> = image::save_buffer(
+        "quizzi_image.png",
+        &new_one_dim,
+        WIDTH as u32,
+        HEIGHT as u32,
+        image::ColorType::Rgb8,
+    );
     match result {
         Ok(_) => println!("Your image is created"),
-        Err(err) => eprintln!("Error saving image: {}",err),
-        
+        Err(err) => eprintln!("Error saving image: {}", err),
     }
+    match scale_image() {
+        Ok(_) => println!("Image scaling completed"),
+        Err(err) => eprintln!("Error scaling image: {}", err),
+    }
+    Ok(())
+}
+fn scale_image() -> Result<(), image::ImageError> {
+    let img = image::open("./quizzi_image.png")?.to_rgb8();
 
+    let original_width = img.width() as usize;
+    let original_height = img.height() as usize;
+
+    let target_width = 150;
+    let target_height = 150;
+
+    let mut processed_img = RgbImage::new(target_width, target_height);
+
+    for x in 0..target_width {
+        for y in 0..target_height {
+            let source_x =
+                (((x as f32 + 0.5) / target_width as f32) * original_width as f32).floor() as u32;
+            let source_y =
+                (((y as f32 + 0.5) / target_height as f32) * original_height as f32).floor() as u32;
+
+            let pixel = img.get_pixel(source_x, source_y);
+            processed_img.put_pixel(x, y, *pixel);
+        }
+    }
+    processed_img.save("processed_image.png")?;
     Ok(())
 }
